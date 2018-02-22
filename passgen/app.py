3fc3839344336dba576937176c40d15520cdbe35
DmitryZaharov <dimans88@mail.ru>
2018-02-23 01:46:43 +0300

    CORS config

--- passgen/app.py
+++ passgen/app.py
@@ -3,7 +3,8 @@ from falcon_cors import CORS
 
 from passgen.passwords.resources import PasswordsResource
 
-cors = CORS(allow_all_origins=True)
+cors = CORS(allow_all_origins=True, allow_all_headers=True,
+            allow_methods_list=['OPTIONS', 'GET'])
 
 application = falcon.API(middleware=[cors.middleware])
 
